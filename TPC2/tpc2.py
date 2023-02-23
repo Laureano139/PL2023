d0e2313752b72037bdbafd77ae06432673d97f50
Hendrix <afamorim16@sapo.pt>
2023-02-23 23:08:17 +0000

    Update tpc2.py

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -1,45 +1,43 @@
 
-import re
-
-def switchState(estado: bool):
-    if estado == 1:
-        estado = 0
-    else:
-        estado = 1
-    return estado
-
-def parseInput(input, estado, soma):
-    
-    if estado:
-        sequenciasDigitos = r"\d+"
-        digitos = re.findall(sequenciasDigitos, input)
-        soma += [int(x) for x in digitos]
-    else:
-        while(ligado not in input):
-            soma += 0 
-    if (desligado in input.upper()):
-        input = input[3:]
-        switchState(estado)
-    if (ligado in input.upper()):
-        input = input[2:]
-        switchState(estado)
-    if (igual in input):
-        input = input[1:]
-        print("Soma atual = " + soma)
-    return soma
-
-
-ligado = "ON"
-desligado = "OFF"
-igual = '='
-
-estado = True
+paradoNoBailao = False
 soma = 0
+x = ''
 
-while True:
-    try:
-        input = input()
-        parseInput(input, estado,soma)
-    except:
-        break
+usr_input = input("Insira o input -> ")
+
+while usr_input != "":
+    
+    if ("on" == usr_input.lower()):
+        paradoNoBailao = False
+    elif ("off" == usr_input.lower()):
+        paradoNoBailao = True
+    elif ('=' in usr_input):
+        print(soma)
+    elif paradoNoBailao == False:
+        n = ""
+        i = 0
         
+        while i < len(usr_input):
+            x = usr_input[i]
+            while i < len(usr_input) and x.isdigit():
+                n += x 
+                i += 1
+                if i < len(usr_input):
+                    x = usr_input[i]
+                
+            if n != "":
+                soma += int(n)
+                n = ""
+                
+            i += 1
+                
+    usr_input = input("Insira o input -> ")
+
+
+
+
+
+
+
+                
+
