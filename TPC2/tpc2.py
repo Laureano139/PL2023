60b5871ff089f1ce85de2e90bbe67d5663a50a86
Hendrix <afamorim16@sapo.pt>
2023-02-23 23:31:55 +0000

    Final

--- TPC2/tpc2.py
+++ TPC2/tpc2.py
@@ -13,9 +13,21 @@ paradoNoBailao = False
 soma = 0
 x = ''
 
+print("Para ajuda e contexto da aplicação, use o comando help\n")
+
 usr_input = input("Insira o input -> ")
 
 while not usr_input == "":
+    
+    if ("help" == usr_input.lower()):
+        print("Este mini-trabalho é baseado num somador com 3 operadores especiais ( on, off, = ).\n"
+           "Para usar esta aplicação basta dar um input (numeros ou letras).\n"
+           "A aplicação irá somar os numeros dados como input, sendo que, se depois colocar o operador =,\n"
+           "irá devolver a soma total dos numeros dados até ao momento.\n"
+           "Se usar o comando off, a aplicação irá ignorar qualquer input que for dado, tanto numeros como operadores \n"
+           "sendo que o valor da soma vai ficar inalterado ao último apresentado.\n"
+           "Se quiser voltar ao normal funcionamento, basta usar o comando on.\n"
+           "Espero que tenha ajudado! :D ")
     
     if ("on" == usr_input.lower()):
         paradoNoBailao = False
